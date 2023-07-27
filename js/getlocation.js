ed4e60e4ccccd1e047c3dc3e34f219b9a5ec5e2a
Hamish MacDonald <h@kor.nz>
2023-07-27 19:08:26 +1200

    Add automatic geolocation if permission already granted.

--- js/getlocation.js
+++ js/getlocation.js
@@ -5,33 +5,46 @@ function GetLocation()
     let btnLocation = document.getElementById('btn-location');
     btnLocation.addEventListener('click', ()=>{ getLocation(); });
     btnLocation.addEventListener('touchstart', ()=>{ getLocation(); });
+
+    navigator.permissions.query({ name: "geolocation" }).then((result) => {
+      if (result.state === "granted")
+      {
+        navigator.geolocation.getCurrentPosition(receivePosition);
+      } 
+      else if (result.state === "denied")
+      {
+        btnLocation.textContent = 'Geolocation permission denied';
+        btnLocation.className = 'overview-button-disabled';
+        btnLocation.disabled = true;
+      }
+    });
   }
 }
 
 this.getLocation = function()
+{
+  let btnLocation = document.getElementById('btn-location');
+  if (navigator.geolocation)
   {
-    let btnLocation = document.getElementById('btn-location');
-    if (navigator.geolocation)
-    {
-      navigator.geolocation.getCurrentPosition(receivePosition);
-      btnLocation.textContent = 'Geolocation requested...';
-      btnLocation.className = 'overview-button-disabled';
-      btnLocation.disabled = true;
-    } 
-    else 
-    {
-      btnLocation.textContent = 'Geolocation not supported';
-      btnLocation.className = 'overview-button-disabled';
-      btnLocation.disabled = true;
-    }
-  }
-
-function receivePosition(position)
-  {
-    let btnLocation = document.getElementById('btn-location');
-    btnLocation.textContent = 'Geolocation enabled!';
+    navigator.geolocation.getCurrentPosition(receivePosition);
+    btnLocation.textContent = 'Check browser permissions...';
     btnLocation.className = 'overview-button-disabled';
     btnLocation.disabled = true;
-    setTimeout(()=> { document.getElementById('btn-location').style.display = 'none'; }, 1000);
-    overview.setLocation(position.coords.latitude, position.coords.longitude);
+  } 
+  else 
+  {
+    btnLocation.textContent = 'Geolocation not supported';
+    btnLocation.className = 'overview-button-disabled';
+    btnLocation.disabled = true;
   }
+}
+
+function receivePosition(position)
+{
+  let btnLocation = document.getElementById('btn-location');
+  btnLocation.textContent = 'Geolocation enabled!';
+  btnLocation.className = 'overview-button-disabled';
+  btnLocation.disabled = true;
+  setTimeout(()=> { document.getElementById('btn-location').style.display = 'none'; }, 1000);
+  overview.setLocation(position.coords.latitude, position.coords.longitude);
+}
