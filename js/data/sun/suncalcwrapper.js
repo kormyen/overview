ec3376c4f47d8a4a4a8a3b3e6889c930e5cab5e2
Hamish MacDonald <h@kor.nz>
2024-12-19 18:36:56 +1300

    Refactor SunData.

--- js/data/sun/suncalcwrapper.js
+++ js/data/sun/suncalcwrapper.js
@@ -21,6 +21,27 @@ function SunCalcWrapper()
     this.result.Set6Night = null;
     this.result.Set7SolarNadir = null;
 
+    this.result.set1Perc = null;
+    this.result.set2Perc = null;
+    this.result.set3Perc = null;
+    this.result.set4Perc = null;
+    this.result.set5Perc = null;
+    this.result.set6Perc = null;
+
+    this.result.rise1Perc = null;
+    this.result.rise2Perc = null;
+    this.result.rise3Perc = null;
+    this.result.rise4Perc = null;
+    this.result.rise5Perc = null;
+    this.result.rise6Perc = null;
+
+    const TOTALSECONDSINDAY = 60*60*24;
+
+    this.getSecondsThroughDay = function(givenDate)
+    {
+        return givenDate.getHours() * 3600 + givenDate.getMinutes() * 60 + givenDate.getSeconds();
+    }
+
     this.updateGregorian = function(date, lat, long)
     {
         date.setHours(12) // ensure SunCalc identifies correct day
@@ -28,45 +49,59 @@ function SunCalcWrapper()
         
         // times.goldenHour	                    evening golden hour starts
         this.result.Set1GoldenHour =            times.goldenHour;
+        this.result.set1Perc =                  this.getSecondsThroughDay(times.goldenHour) / TOTALSECONDSINDAY;
 
         // times.sunsetStart	                sunset starts (bottom edge of the sun touches the horizon)
         this.result.Set2Sunset =                times.sunsetStart;
+        this.result.set2Perc =                  this.getSecondsThroughDay(times.sunsetStart) / TOTALSECONDSINDAY;
 
         // times.sunset	                        sunset (sun disappears below the horizon, evening civil twilight starts)
         this.result.Set3TwilightCivil =         times.sunset;
+        this.result.set3Perc =                  this.getSecondsThroughDay(times.sunset) / TOTALSECONDSINDAY;
 
         // times.dusk	                        dusk (evening nautical twilight starts)
         this.result.Set4TwilightNautical =      times.dusk;
+        this.result.set4Perc =                  this.getSecondsThroughDay(times.dusk) / TOTALSECONDSINDAY;
 
         // times.nauticalDusk	                nautical dusk (evening astronomical twilight starts)
         this.result.Set5TwilightAstro =         times.nauticalDusk;
+        this.result.set5Perc =                  this.getSecondsThroughDay(times.nauticalDusk) / TOTALSECONDSINDAY;
 
         // times.night	                        night starts (dark enough for astronomical observations)
         this.result.Set6Night =                 times.night;
+        this.result.set6Perc =                  this.getSecondsThroughDay(times.night) / TOTALSECONDSINDAY;
 
         // times.nadir	                        nadir (darkest moment of the night, sun is in the lowest position)
         this.result.Set7SolarNadir =            times.nadir;
+        this.set7Perc =                         this.getSecondsThroughDay(times.nadir) / TOTALSECONDSINDAY;
 
 
         // times.nightEnd	                    night ends (morning astronomical twilight starts)
         this.result.Rise1TwilightAstro =        times.nightEnd;
+        this.result.rise1Perc =                 this.getSecondsThroughDay(times.nightEnd) / TOTALSECONDSINDAY;
 
         // times.nauticalDawn	                nautical dawn (morning nautical twilight starts)
         this.result.Rise2TwilightNautical =     times.nauticalDawn;
+        this.result.rise2Perc =                 this.getSecondsThroughDay(times.nauticalDawn) / TOTALSECONDSINDAY;
 
         // times.dawn	                        dawn (morning nautical twilight ends, morning civil twilight starts)
         this.result.Rise3TwilightCivil =        times.dawn;
+        this.result.rise3Perc =                 this.getSecondsThroughDay(times.dawn) / TOTALSECONDSINDAY;
 
         // times.sunrise	                    sunrise (top edge of the sun appears on the horizon)
         this.result.Rise4Sunrise =              times.sunrise;
+        this.result.rise4Perc =                 this.getSecondsThroughDay(times.sunrise) / TOTALSECONDSINDAY;
         
         // times.sunriseEnd	                    sunrise ends (bottom edge of the sun touches the horizon)
         this.result.Rise5GoldenHour =           times.sunriseEnd;
+        this.result.rise5Perc =                 this.getSecondsThroughDay(times.sunriseEnd) / TOTALSECONDSINDAY;
         
         // times.goldenHourEnd	                morning golden hour (soft light, best time for photography) ends
         this.result.Rise6Day =                  times.goldenHourEnd;
+        this.result.rise6Perc =                 this.getSecondsThroughDay(times.goldenHourEnd) / TOTALSECONDSINDAY;
 
         // times.solarNoon	                    solar noon (sun is in the highest position)
         this.result.Rise7SolarNoon =            times.solarNoon;
+        this.result.rise7Perc =                 this.getSecondsThroughDay(times.solarNoon) / TOTALSECONDSINDAY; 
     }
 }
--- js/draw/drawsunlight.js
+++ js/draw/drawsunlight.js
@@ -14,30 +14,12 @@ function DrawSunlight(drawShared)
             currentGraduationDegrees += 180;
         }
 
-        const TOTALSECONDSINDAY = 60*60*24;
-        
-        let set1Perc = this.getSecondsThroughDay(sunData.Set1GoldenHour) / TOTALSECONDSINDAY;
-        let set2Perc = this.getSecondsThroughDay(sunData.Set2Sunset) / TOTALSECONDSINDAY;
-        let set3Perc = this.getSecondsThroughDay(sunData.Set3TwilightCivil) / TOTALSECONDSINDAY;
-        let set4Perc = this.getSecondsThroughDay(sunData.Set4TwilightNautical) / TOTALSECONDSINDAY;
-        let set5Perc = this.getSecondsThroughDay(sunData.Set5TwilightAstro) / TOTALSECONDSINDAY;
-        let set6Perc = this.getSecondsThroughDay(sunData.Set6Night) / TOTALSECONDSINDAY;
-        // let set7Perc = this.getSecondsThroughDay(sunData.Set7SolarNadir) / TOTALSECONDSINDAY;
-
-        let rise1Perc = this.getSecondsThroughDay(sunData.Rise1TwilightAstro) / TOTALSECONDSINDAY;
-        let rise2Perc = this.getSecondsThroughDay(sunData.Rise2TwilightNautical) / TOTALSECONDSINDAY;
-        let rise3Perc = this.getSecondsThroughDay(sunData.Rise3TwilightCivil) / TOTALSECONDSINDAY;
-        let rise4Perc = this.getSecondsThroughDay(sunData.Rise4Sunrise) / TOTALSECONDSINDAY;
-        let rise5Perc = this.getSecondsThroughDay(sunData.Rise5GoldenHour) / TOTALSECONDSINDAY;
-        let rise6Perc = this.getSecondsThroughDay(sunData.Rise6Day) / TOTALSECONDSINDAY;
-        // let rise7Perc = this.getSecondsThroughDay(sunData.Rise7SolarNoon) / TOTALSECONDSINDAY; 
-
-        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, set6Perc, rise1Perc, 0.95, colorSecondary, colorBackground, earthSize); // astro
-        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, set5Perc, rise2Perc, 0.85, colorSecondary, colorBackground, earthSize); // nautical
-        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, set4Perc, rise3Perc, 0.75, colorSecondary, colorBackground, earthSize); // civil
-        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, set3Perc, rise4Perc, 0.2, colorSecondary, colorBackground, earthSize); // sunrise
-        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, set2Perc, rise5Perc, 0.4, colorSecondary, colorBackground, earthSize); // golden hour
-        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, set1Perc, rise6Perc, 0.5, colorSecondary, colorBackground, earthSize); // day
+        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, sunData.set6Perc, sunData.rise1Perc, 0.95, colorSecondary, colorBackground, earthSize); // astro
+        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, sunData.set5Perc, sunData.rise2Perc, 0.85, colorSecondary, colorBackground, earthSize); // nautical
+        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, sunData.set4Perc, sunData.rise3Perc, 0.75, colorSecondary, colorBackground, earthSize); // civil
+        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, sunData.set3Perc, sunData.rise4Perc, 0.2, colorSecondary, colorBackground, earthSize); // sunrise
+        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, sunData.set2Perc, sunData.rise5Perc, 0.4, colorSecondary, colorBackground, earthSize); // golden hour
+        this.drawSunLightArc(context, cx, cy, currentGraduationDegrees, sunData.set1Perc, sunData.rise6Perc, 0.5, colorSecondary, colorBackground, earthSize); // day
     }
 
     this.drawSunLightArc = function(context, cx, cy, currentGraduationDegrees, startPerc, endPerc, colorPerc, colorSecondary, colorBackground, earthSize)
@@ -54,11 +36,6 @@ function DrawSunlight(drawShared)
         context.arc(cx, cy, earthSize, eAngle, sAngle, false);
         context.closePath();
         context.fill();
-    }
-
-    this.getSecondsThroughDay = function(givenDate)
-    {
-        return givenDate.getHours() * 3600 + givenDate.getMinutes() * 60 + givenDate.getSeconds();
     }
 
     this.degreesToRadians = function(degrees)
